ff120a88bfd4215619b1c7e885d0b377bbceae58
Praval Gautam <pravalgautam79@gmail.com>
2024-08-06 14:01:07 +0530

    Initial Commit

---
+++ Availability/AvailabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvailabilityApp.swift
+//  Availability
+//
+//  Created by Praval Gautam on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AvailabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
